b873b90bf3bf7cbb3bc41907138c0f366f9b90e3
Alisson Gabriel <ali2slima10@gmail.com>
2022-09-11 13:53:41 -0300

    Update etl.py

--- Project 3 Data Warehouse/etl.py
+++ Project 3 Data Warehouse/etl.py
@@ -4,18 +4,27 @@ from sql_queries import copy_table_queries, insert_table_queries
 
 
 def load_staging_tables(cur, conn):
+    """
+    Load data from files stored in S3 to the staging tables.
+    """
     for query in copy_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def insert_tables(cur, conn):
+    """
+    Insert data from staging tables into the tables.
+    """
     for query in insert_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def main():
+    """
+    Extract song metadata and user activity data from S3, transform it using a staging table, and load it into fact and dimensional tables for analysis
+    """
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
 
