9f28ef71614fd8257180334f8812719f64dfa1f1
Alisson Gabriel <ali2slima10@gmail.com>
2022-09-11 13:52:46 -0300

    Update create_tables.py

--- Project 3 Data Warehouse/create_tables.py
+++ Project 3 Data Warehouse/create_tables.py
@@ -4,18 +4,31 @@ from sql_queries import create_table_queries, drop_table_queries
 
 
 def drop_tables(cur, conn):
+    """
+    Drop all the table in the Redshift cluster
+    :param cur: cursor object to database connection
+    :param conn: connection object to database
+    """
     for query in drop_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def create_tables(cur, conn):
+    """
+    Create all the tables in the Redshift cluster
+    :param cur: cursor object to database connection
+    :param conn: connection object to database
+    """
     for query in create_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def main():
+    """
+    Connects to the Redshift cluster and resets the tables.
+    """
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
 
